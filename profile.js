68365caed3ff9544c0c715c5a66579430d93fe57
Phillip Avila <Anthonyavilz@gmail.com>
2023-05-12 11:52:34 -0500

    finished week-3

---
+++ profile.js
@@ -0,0 +1,11 @@
+// Color
+document.querySelector('#color').addEventListener('click', () => alert('My favorite color is Midnight Blue'))
+
+
+// Place
+document.querySelector('#place').addEventListener('click', () => alert('My favorite place is Chicago'))
+
+
+
+// Ritual
+document.querySelector('#ritual').addEventListener('click', () => alert('My favorite ritual is to workout'))
